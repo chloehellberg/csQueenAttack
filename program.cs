669fee04852db2da6c2408373f8f4282f67917ef
Mike Manchee <mikemanchee@gmail.com>
2020-09-21 14:48:29 -0700

    add basic file structure and program.cs basics
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

---
+++ program.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Chess {
+
+  public class Program
+  {
+    public static void Main()
+    {
+      
+      // ask for queen coordinate 1
+      Console.WriteLine("What is the Queen's vertical coordinate is (1-9)");
+      string queenV = Console.ReadLine();
+      // ask for queen coordinate 2
+      Console.WriteLine("What is the Queen's horizontal coordinate is (a-h)");
+      string queenH = Console.ReadLine();
+      // ask for enemy coordinate 1
+      Console.WriteLine("What is the Enemy's vertical coordinate is (1-9)");
+      string enemyV = Console.ReadLine();
+      // ask for enemy coordinate 2
+      Console.WriteLine("What is the Enemy's horizontal coordinate is (a-h)");
+      string enemyH = Console.ReadLine();
+
+      string[] coordinates = {queenV, queenH, enemyV, enemyH};
+      // send coordinates to business logic
+      bool result = 
+      // return true or false
+
+    }
+  } 
+}
