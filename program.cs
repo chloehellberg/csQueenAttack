faba1cc68844a3d339a22f90fdfa5725fcb60e6f
Mike Manchee <mikemanchee@gmail.com>
2020-09-21 17:15:19 -0700

    refactor function to make it smaller using slope
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- program.cs
+++ program.cs
@@ -9,19 +9,23 @@ namespace Chess {
     {
       Queen moves = new Queen();
       // ask for queen coordinate 1
-      Console.WriteLine("What is the Queen's vertical coordinate is (1-9)");
-      string queenV = Console.ReadLine();
+      Console.WriteLine("What is the Queen's vertical coordinate is (1-8)");
+      string stringQueenV = Console.ReadLine();
+      int queenV = int.Parse(stringQueenV);
       // ask for queen coordinate 2
-      Console.WriteLine("What is the Queen's horizontal coordinate is (a-h)");
-      string queenH = Console.ReadLine();
+      Console.WriteLine("What is the Queen's horizontal coordinate is (1-8)");
+      string stringQueenH = Console.ReadLine();
+      int queenH = int.Parse(stringQueenH);
       // ask for enemy coordinate 1
-      Console.WriteLine("What is the Enemy's vertical coordinate is (1-9)");
-      string enemyV = Console.ReadLine();
+      Console.WriteLine("What is the Enemy's vertical coordinate is (1-8)");
+      string stringEnemyV = Console.ReadLine();
+      int enemyV = int.Parse(stringEnemyV);
       // ask for enemy coordinate 2
-      Console.WriteLine("What is the Enemy's horizontal coordinate is (a-h)");
-      string enemyH = Console.ReadLine();
+      Console.WriteLine("What is the Enemy's horizontal coordinate is (1-8)");
+      string stringEnemyH = Console.ReadLine();
+      int enemyH = int.Parse(stringEnemyH);
 
-      string[] coordinates = {queenV, queenH, enemyV, enemyH};
+      int[] coordinates = {queenV, queenH, enemyV, enemyH};
       // send coordinates to business logic
       bool result = moves.GetMove(coordinates);
       // return true or false
